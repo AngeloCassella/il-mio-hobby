18d3404bcda6f21694683b338e9ddaf5aa9c7925
Angelo Cassella <angel.cass92@gmail.com>
2024-03-11 18:53:31 +0100

    marge con branch gabriele

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Mark from './components/Mark';
-import Title from './components/Title';
-
-=======
 import Titolo from './components/Titolo.jsx'
 import Utente from './components/Utente.jsx'
 import Ospite from './components/Ospite.jsx'
->>>>>>> 57047cd31eeac91229d1cfe3e9498d97dd17091c
-
+import Mark from './components/Mark';
 import './App.css'
+
 
 function App () {
   const utente = localStorage.getItem('utente')
@@ -21,29 +14,10 @@ function App () {
     UserComponent = <Utente nome={utente} />
   }
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Mark backgroundColor="yellow"> scritta evidenziata gilla</Mark>
-    <Title/>
-=======
     <div className='page'>
       <Titolo>il mio Hobby</Titolo>
+      <Mark backgroundColor="yellow"> scritta evidenziata gilla</Mark>
       {UserComponent}
->>>>>>> 57047cd31eeac91229d1cfe3e9498d97dd17091c
     </div>
   )
 }
--- src/components/Title.jsx
+++
@@ -1,6 +0,0 @@
-function Title() {
-    return <h1 style={{color:"red"}}>lezione 30</h1>;
-    
-}
-
-export default Title;
