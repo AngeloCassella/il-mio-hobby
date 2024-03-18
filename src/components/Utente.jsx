451119c3a1b81de1a61cc1525c326a33a5d91b10
Angelo Cassella <angel.cass92@gmail.com>
2024-03-18 18:48:45 +0100

    aggiornamento dati

--- src/components/Utente.jsx
+++ src/components/Utente.jsx
@@ -1,7 +1,7 @@
 
 function Utente ({ nome, isUtente = false }) {
   if(isUtente) {
-    return <h2>Benvenuto {(nome === 'gabriele') ? 'GABRIELE' : nome}</h2>
+    return <h2>Benvenuto {(nome === 'angelo') ? 'GABRIELE' : nome}</h2>
   }
   return <p style={{fontFamily: "Helvetica, Arial"}}>{nome}</p>
 }
